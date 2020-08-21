ebc332ce13590453cca8b85bfe5c18199c87f6d7
João Aurélio Francisco Junior <47570219+joaoJunior174@users.noreply.github.com>
2020-08-21 19:22:59 -0300

    Add files via upload

---
+++ Tarefa5/controllers/my_controller/build/release/my_controller.d
@@ -0,0 +1,7 @@
+build/release/my_controller.o: my_controller.c \
+ C:\Users\THERMALTAKE\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\THERMALTAKE\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\THERMALTAKE\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\THERMALTAKE\AppData\Local\Programs\Webots/include/controller/c/webots/position_sensor.h \
+ C:\Users\THERMALTAKE\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\THERMALTAKE\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
---
+++ Tarefa5/controllers/my_controller/my_controller.c
@@ -0,0 +1,125 @@
+#include <webots/distance_sensor.h>
+#include <webots/motor.h>
+#include <webots/position_sensor.h>
+#include <webots/robot.h>
+#include <math.h>
+#include <stdio.h>
+
+#define TIME_STEP 64
+
+void delay (int time_milisec) {
+  double currentTime, initTime, Timeleft;
+  double timeValue = (double)time_milisec/1000;
+  initTime = wb_robot_get_time();
+  Timeleft = 0.00;
+  while (Timeleft < timeValue)
+  {
+    currentTime = wb_robot_get_time();
+    Timeleft = currentTime - initTime;
+    wb_robot_step(TIME_STEP);
+  }
+}
+
+
+double mudarSinal(double Ox){
+
+   if(Ox < 0){
+     
+      return Ox+2*M_PI;
+   
+   }
+    return Ox;
+   
+}
+
+double mudarSinalInverso(double Ox){
+
+   if(Ox > 0){
+     
+      return Ox-2*M_PI;
+   
+   }
+    return Ox;
+   
+}
+
+double ajustarAngulos(double Ox, double min, double max){
+
+  if(Ox < min){
+     return min;
+  }if(Ox > max){
+  
+    return max;
+  }
+  
+  return Ox;
+
+}
+
+void calcularCinematica(double x, double y, double z, double theta[3]){
+    double l1 = 0.5;
+    double l2 = 2;
+    double l3 = 2;
+    double R1 = sqrt(pow(x,2) + pow(y,2));
+    double R2 = z-l1;//aqui
+    double R3 =  sqrt(pow(R1,2) + pow(R2,2));
+    double O1 = atan(y/x);//aqui
+
+    double aux =  (pow(l3,2) - pow(l2,2) - pow(R3,2)) / (-2*l2*R3);
+    double aux2 =  (pow(R3,2) - pow(l2,2) - pow(l3,2)) / (-2*l2*l3);
+    
+    aux = ajustarAngulos(aux,-1,1);
+    aux2 = ajustarAngulos(aux2,-1,1);
+    
+    double O2 = atan(R2/R1) - acos(aux);
+    double O3 = M_PI - acos(aux2);
+    
+    O1 = ajustarAngulos(mudarSinal(O1),0,6.03);
+    O2 = ajustarAngulos(mudarSinalInverso(O2),-2.44,0);
+    O3 = ajustarAngulos(mudarSinal(O3),0.0,4.21);
+    
+    theta[0]=O1;
+    theta[1]=O2;
+    theta[2]=O3;
+}
+
+int main(int argc, char **argv) {
+  
+  wb_robot_init();
+  
+  double posicao_final1[3] = {-0.08, 0.38, 0.27};
+  double posicao_final2[3] = {-0.08, 0.19,-0.34};
+  double posicao_final3[3] = {0.3, 0.36, 0.0};
+  double theta[3];
+  
+  WbDeviceTag motors[3];
+  motors[0] = wb_robot_get_device("base");
+  motors[1] = wb_robot_get_device("upperarm");
+  motors[2] = wb_robot_get_device("forearm");
+
+  
+  
+  for (int i = 0; i < 3; i++)
+    wb_motor_set_velocity(motors[i], 1.0);
+  
+
+  calcularCinematica(posicao_final3[0], posicao_final3[1], posicao_final3[2], theta);
+  wb_motor_set_position(motors[0], theta[0]);
+  wb_motor_set_position(motors[1], theta[1]);
+  wb_motor_set_position(motors[2], theta[2]);
+  delay(3000);
+  calcularCinematica(posicao_final2[0], posicao_final2[1], posicao_final2[2], theta);
+  wb_motor_set_position(motors[0], theta[0]);
+  wb_motor_set_position(motors[1], theta[1]);
+  wb_motor_set_position(motors[2], theta[2]);
+  delay(3000);
+  calcularCinematica(posicao_final1[0], posicao_final1[1], posicao_final1[2], theta);
+  wb_motor_set_position(motors[0], theta[0]);
+  wb_motor_set_position(motors[1], theta[1]);
+  wb_motor_set_position(motors[2], theta[2]); 
+  delay(3000);
+
+  wb_robot_cleanup();
+
+  return 0;
+}
